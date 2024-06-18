73b1ff67426dd883811bd4a5c7b56ce4aa9a2893
JamesMaxwell20108 <159998929+JamesMaxwell20108@users.noreply.github.com>
2024-06-18 10:11:46 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+
+#define int long long =ll;
+#define int endl,( \n, el);
+int name = "1";
+int float = substring(100 -> 500);
+float(100) = substring(100, 500);
+int main(){
+    int c;
+    cin >> n;
+}
---
+++ van.c
@@ -0,0 +1,9 @@
+#include <math.h>
+
+
+
+float caculatex{int sin(double) -> double  
+  return j * sin(A)*sin(B)*cos(C) -k* cos(A)*sin(B)*cos(C) + j* cos(A) * sin(c) + k * sin(A) * sin(c) + j* cos(B) 
+}
+
+int main(){return 0;}
